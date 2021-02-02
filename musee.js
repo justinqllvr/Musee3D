9f1ada8206dadec35f6886170023dac33f613c84
evanmartiin <evan.martiin29@gmail.com>
2021-02-02 13:32:49 +0100

    ajout musée

--- musee.js
+++ musee.js
@@ -1,53 +1,173 @@
+var canDisplay = false;
 var canvas = document.getElementById("musee");
-
 var engine = new BABYLON.Engine(canvas, true);
-
-
 
 var createScene = function () {
 
     var scene = new BABYLON.Scene(engine); //On crée une nouvelle scène
-    scene.clearColor = new BABYLON.Color3(0, 0.1, 0); //Couleur par défaut quand il n'y a pas d'élement (soit la couleur au fond)
+    scene.clearColor = new BABYLON.Color3(0.8, 0.2, 0.2); //Couleur par défaut quand il n'y a pas d'élement (soit la couleur au fond)
 
     //Création d'une caméra avec les paramètres : string nom_camera, Vector3 position, scene
-    var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 3, -10), scene); //0, 3, -10
-
-    camera.setTarget(BABYLON.Vector3.Zero()); //On fait regarder la caméra vers l'origine de la scène.
-
+    var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 3, -15), scene);
+    camera.setTarget(new BABYLON.Vector3(0, 0, -70)); //On fait regarder la caméra vers l'origine de la scène.
     camera.attachControl(canvas, false); //On attache les contrôles de la scène via le canvas qu'on a crée tout à l'heure dans musee.html
-
-
-    //gestion des contrôles 
     camera.keysLeft = [81, 37]; //Q et <-
     camera.keysUp = [90, 38]; //Z et Flèche du haut
     camera.keysRight = [68, 39]; //D et ->
     camera.keysDown = [83, 40]; //S et flèche du bas
-
-    camera.speed = 1;
-
-    camera.checkCollisions = true;
-
-    scene.gravity = new BABYLON.Vector3(0, 0, 0);
+    camera.speed = 0.5;
     camera.applyGravity = true;
+    camera.checkCollisions = true;
+    camera.angularSensibility = 5000;
+    camera.position = new BABYLON.Vector3(-1, 2, 80);
 
-    // //appliquer un effet à la caméra 
-    // var postProcess0 = new BABYLON.BlurPostProcess("Horizontal blur", new BABYLON.Vector2(1.0, 0), 0, 1.0, camera);
-    //gestion du curseur 
+    var hitbox = BABYLON.Mesh.CreateSphere("hitbox", 16, 1, scene);
+    var mat_hitbox = new BABYLON.StandardMaterial("hitbox", scene);
+    mat_hitbox.alpha = 0;
+    hitbox.material = mat_hitbox;
 
-    var pointerlockchange = function () {
-        var controlEnabled = document.mozPointerLockElement || document.webkitPointerLockElement || document.msPointerLockElement || document.pointerLockElement || false;
-        if (!controlEnabled) {
-            camera.detachControl(canvas);
-            islocked = false;
-        }
-        else {
-            camera.attachControl(canvas);
-            islocked = true;
-        }
-    };
+    //Paramètres : string nom, Vector3 orientation_lumière, scene
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene); //On crée une nouvelle lumière générale orientée vers le haut
+    light.intensity = 0.8; //On diminue un peu l'intensité de la lumière
 
-    camera.angularSensibility = 10000;
+    // var sphere3 = BABYLON.Mesh.CreateSphere("sphere3", 20, 4, scene);
+    // sphere3.position = new BABYLON.Vector3(0, 3, 0);
+    // var mat_sphere3 = new BABYLON.StandardMaterial("sphere3", scene);
+    // sphere3.material = mat_sphere3;
 
+    // var sphereSound = BABYLON.Mesh.CreateSphere("sphereSound", 20, 4, scene);
+    // sphereSound.position = new BABYLON.Vector3(50, 3, -30);
+
+    // var mePerdonas = new BABYLON.Sound("mePerdonas", "sound/sound.mp3", scene, null, {
+    //     loop: true,
+    //     autoplay: true,
+    //     maxDistance: 30
+    // });
+    // mePerdonas.attachToMesh(sphereSound);
+    // var zoneSound = BABYLON.MeshBuilder.CreatePlane("zoneSound", { height: 16, width: 16, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    // zoneSound.position = new BABYLON.Vector3(50, 0.2, -30);
+    // zoneSound.rotation.x = 1.57;
+    // var zoneSound_mat = new BABYLON.StandardMaterial("zoneSound_mat", scene);
+    // zoneSound_mat.alpha = 0.5;
+    // zoneSound.material = zoneSound_mat;
+
+    //Paramètres : string nom, int longueur (axe x), int largeur (axe z), int sous-divisions, scene
+    var ground = BABYLON.Mesh.CreateGround("ground1", 250, 250, 2, scene); //On crée un sol (petit certes) qui servira plus tard
+    var mat_sol = new BABYLON.StandardMaterial("sol", scene);
+    mat_sol.diffuseTexture = new BABYLON.Texture("texture/sol.jpg", scene);
+    mat_sol.bumpTexture = new BABYLON.Texture("texture/sol_bump.jpg", scene);
+    mat_sol.diffuseTexture.uScale = 16.0 * 1.5;
+    mat_sol.diffuseTexture.vScale = 16.0;
+    mat_sol.bumpTexture.uScale = 16.0 * 1.5;
+    mat_sol.bumpTexture.vScale = 16.0;
+    ground.checkCollisions = true;
+    ground.material = mat_sol;
+
+
+
+
+
+
+
+    // Peintures principales (avec interaction)
+
+    var fransHals = BABYLON.MeshBuilder.CreatePlane("fransHals", { height: 3.75, width: 3.12, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    fransHals.position = new BABYLON.Vector3(-1, 3.5, 63.8);
+    fransHals.rotation.y = 3.14;
+    var fransHalsMAT = new BABYLON.StandardMaterial("fransHalsMAT", scene);
+    fransHalsMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/franshals.jpg", scene, false);
+    fransHals.material = fransHalsMAT;
+
+    var anonymeFlamand = BABYLON.MeshBuilder.CreatePlane("anonymeFlamand", { height: 6.54, width: 5.4, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    anonymeFlamand.position = new BABYLON.Vector3(-1, 3.5, 25.7);
+    anonymeFlamand.rotation.y = 3.14;
+    var anonymeFlamandMAT = new BABYLON.StandardMaterial("anonymeFlamandMAT", scene);
+    anonymeFlamandMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/anonymeflamand.jpg", scene, false);
+    anonymeFlamand.material = anonymeFlamandMAT;
+
+    var hendrickTerBrugghen = BABYLON.MeshBuilder.CreatePlane("hendrickTerBrugghen", { height: 6.27, width: 5.08, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    hendrickTerBrugghen.position = new BABYLON.Vector3(-13.7, 3.5, 32.5);
+    hendrickTerBrugghen.rotation.y = 1.57;
+    var hendrickTerBrugghenMAT = new BABYLON.StandardMaterial("hendrickTerBrugghenMAT", scene);
+    hendrickTerBrugghenMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/hendrickterbrugghen.jpeg", scene, false);
+    hendrickTerBrugghen.material = hendrickTerBrugghenMAT;
+
+    var vanGoyen = BABYLON.MeshBuilder.CreatePlane("vanGoyen", { height: 2.94, width: 4.14, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    vanGoyen.position = new BABYLON.Vector3(-14, 3.5, 5.1);
+    vanGoyen.rotation.y = 3.14;
+    var vanGoyenMAT = new BABYLON.StandardMaterial("vanGoyenMAT", scene);
+    vanGoyenMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/janvangoyen.jpeg", scene, false);
+    vanGoyen.material = vanGoyenMAT;
+
+    var melchiorDhondecoeter = BABYLON.MeshBuilder.CreatePlane("melchiorDhondecoeter", { height: 4.08, width: 3.3, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    melchiorDhondecoeter.position = new BABYLON.Vector3(-24.6, 4, -13);
+    melchiorDhondecoeter.rotation.y = -1.57;
+    var melchiorDhondecoeterMAT = new BABYLON.StandardMaterial("melchiorDhondecoeterMAT", scene);
+    melchiorDhondecoeterMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/melchiordhondecoeter.jpg", scene, false);
+    melchiorDhondecoeter.material = melchiorDhondecoeterMAT;
+
+    var bakhuizen = BABYLON.MeshBuilder.CreatePlane("bakhuizen", { height: 5.04, width: 5.04, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    bakhuizen.position = new BABYLON.Vector3(8.1, 3.5, -29);
+    bakhuizen.rotation.y = 1.57;
+    var bakhuizenMAT = new BABYLON.StandardMaterial("bakhuizenMAT", scene);
+    bakhuizenMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/bakhuizen.jpg", scene, false);
+    bakhuizen.material = bakhuizenMAT;
+
+    var courrege = BABYLON.MeshBuilder.CreatePlane("courrege", { height: 7.8, width: 11.76, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    courrege.position = new BABYLON.Vector3(-1, 5, -41);
+    courrege.rotation.y = 3.14;
+    var courregeMAT = new BABYLON.StandardMaterial("courregeMAT", scene);
+    courregeMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/courrege.jpg", scene, false);
+    courrege.material = courregeMAT;
+
+    var maitreChandelle = BABYLON.MeshBuilder.CreatePlane("maitreChandelle", { height: 7.78, width: 10.2, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    maitreChandelle.position = new BABYLON.Vector3(-12.4, 4.5, -58);
+    maitreChandelle.rotation.y = -1.57;
+    var maitreChandelleMAT = new BABYLON.StandardMaterial("maitreChandelleMAT", scene);
+    maitreChandelleMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/chandelle.jpg", scene, false);
+    maitreChandelle.material = maitreChandelleMAT;
+
+    var pierrePaulRubens = BABYLON.MeshBuilder.CreatePlane("pierrePaulRubens", { height: 10.2, width: 10.3, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    pierrePaulRubens.position = new BABYLON.Vector3(-1, 5.5, -92.8);
+    pierrePaulRubens.rotation.y = 3.14;
+    var pierrePaulRubensMAT = new BABYLON.StandardMaterial("pierrePaulRubensMAT", scene);
+    pierrePaulRubensMAT.diffuseTexture = new BABYLON.Texture("texture/paintings/main/pierrepaulrubens.jpg", scene, false);
+    pierrePaulRubens.material = pierrePaulRubensMAT;
+
+
+
+
+
+
+
+
+    var lumJeuExpo = new BABYLON.SpotLight("SpotJV", new BABYLON.Vector3(0, 15, 15), new BABYLON.Vector3(0, -1, 0), 0.9, 2, scene);
+    lumJeuExpo.intensity = 2;
+    lumJeuExpo.diffuse = new BABYLON.Color3(0.5, 0.5, 0);
+
+    var skybox = BABYLON.Mesh.CreateBox("skybox", 300, scene);
+    var skyboxmat = new BABYLON.StandardMaterial("m_skybox", scene);
+    skyboxmat.backFaceCulling = false;
+    skyboxmat.disableLightning = true;
+    skyboxmat.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxmat.specularColor = new BABYLON.Color3(0, 0, 0);
+    skyboxmat.reflectionTexture = new BABYLON.CubeTexture("texture/skybox/skybox", scene);
+    skyboxmat.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skybox.material = skyboxmat;
+    skybox.infiniteDistance = true;
+
+    // var sphereModal = BABYLON.Mesh.CreateSphere("sphereModal", 16, 14, scene);
+    // sphereModal.position = new BABYLON.Vector3(0, 0, 15);
+    // var invisibleMat = new BABYLON.StandardMaterial("invisible", scene);
+    // invisibleMat.alpha = 0;
+    // sphereModal.material = invisibleMat;
+
+    // var sphereModalTest = BABYLON.MeshBuilder.CreatePlane("sphereModalTest", { height: 16, width: 16, sideOrientation: BABYLON.Mesh.DOUBLESIDE }, scene);
+    // sphereModalTest.position = new BABYLON.Vector3(0, 0.2, 0);
+    // sphereModalTest.rotation.x = 1.57;
+    // var invisibleMat = new BABYLON.StandardMaterial("invisible", scene);
+    // invisibleMat.alpha = 0.5;
+    // sphereModalTest.material = invisibleMat;
 
     var islocked = false;
     scene.onPointerDown = function (evt) {
@@ -67,87 +187,83 @@ var createScene = function () {
     document.addEventListener("mozpointerlockchange", pointerlockchange, false);
     document.addEventListener("webkitpointerlockchange", pointerlockchange, false);
 
-    //Création de l'enceinte 
-    var loader = new BABYLON.AssetsManager(scene);
-    var salle = loader.addMeshTask("nom", "", "obj/", "museev4.glb");
+    var pointerlockchange = function () {
+        var controlEnabled = document.mozPointerLockElement || document.webkitPointerLockElement || document.msPointerLockElement || document.pointerLockElement || false;
+        if (!controlEnabled) {
+            camera.detachControl(canvas);
+            islocked = false;
+        }
+        else {
+            camera.attachControl(canvas);
+            islocked = true;
+        }
+    };
 
+    var loader = new BABYLON.AssetsManager(scene);
+    var salle = loader.addMeshTask("nom", "", "obj/", "museev11.obj");
     salle.onSuccess = function (t) {
         t.loadedMeshes.forEach(function (m) { //On édite ici chaque maillage de l'objet
             m.position.y = 0; //Pour le monter en hauteur
-            m.position.x = 10; //Et on le décale un peu pour que quand on commence, il soit bien placé.
+            m.position.x = 0; //Et on le décale un peu pour que quand on commence, il soit bien placé.
             m.checkCollisions = true; //Ajout de la détection des collisions, expliqué plus tard.
-            m.material = mat_sol;
+            m.material = new BABYLON.StandardMaterial("salle", scene);
+            m.material.backFaceCulling = false;
         });
     };
 
     loader.load();
 
+    var isShading = false;
 
-    //Gestion des collisions par une sphere crée sur nous même
-    var hitbox = BABYLON.Mesh.CreateSphere("hitbox", 10, 10, scene);
-    hitbox.position = new BABYLON.Vector3(0, 3, -10);
-    hitbox.parent = camera;
-    hitbox.checkCollisions = true;
-    //Paramètres : string nom, Vector3 orientation_lumière, scene
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene); //On crée une nouvelle lumière générale orientée vers le haut
-
-    light.intensity = .5; //On diminue un peu l'intensité de la lumière
-
-    //Paramètres : string nom, int nombre_sous-divisions, int diamètre, scene
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene); //On rajoute une sphère pour voir du visuel (et qui sera le centre de la salle d'exposition).
-
-    sphere.position.y = 1; //On fait monter un peu la sphère.
-
-    sphere.checkCollisions = true;
-
-    sphere.alpha = 0.1;
-    var materialFilFer = new BABYLON.StandardMaterial("FildeFer", scene);
-    materialFilFer.wireframe = true;
-    sphere.material = materialFilFer;
-
-    //Paramètres : string nom, int longueur (axe x), int largeur (axe z), int sous-divisions, scene
-    var ground = BABYLON.Mesh.CreateGround("ground1", 100, 100, 2, scene); //On crée un sol (petit certes) qui servira plus tard
-    ground.checkCollisions = true;
-
-    //gestion des textures et matériaux
-    var mat_sol = new BABYLON.StandardMaterial("sol", scene);
-    mat_sol.diffuseTexture = new BABYLON.Texture("texture/sol.png", scene);
-    mat_sol.bumpTexture = new BABYLON.Texture("texture/sol2.jpg", scene);
-    ground.material = mat_sol;
-
-    //permet de resize les textures
-    mat_sol.diffuseTexture.uScale = 16.0 * 1.5;
-    mat_sol.diffuseTexture.vScale = 16.0;
-    mat_sol.bumpTexture.uScale = 16.0 * 1.5;
-    mat_sol.bumpTexture.vScale = 16.0;
-
-    //créé une vidéo
-    var videoMesh = BABYLON.MeshBuilder.CreatePlane("videoMesh", {height: 16, width: 9}, scene);
-    var videoMat = new BABYLON.StandardMaterial("vidFac", scene);
-    videoMat.diffuseTexture = new BABYLON.VideoTexture("videosFac", ["texture/video/Motion.mp4"], scene, false);
-    videoMat.backFaceCulling = false; //active l'option de texture en regardant par derrière
-    videoMat.diffuseColor = new BABYLON.Color3(1, 1, 1); //couleur pour la video
-    videoMat.specularColor = new BABYLON.Color3(0, 0, 0); //reflets pour la video
-    videoMat.diffuseTexture.video.loop = true; //on applique le matériau à l'objet + on joue en boucle la vidéo
-    videoMesh.material = videoMat;
-    videoMat.diffuseTexture.uScale = 1;
-    videoMat.diffuseTexture.vScale = 2;
-    //permet de faire tourner la sphere
-    scene.beforeRender = function() {
-        sphere.rotation.x -= 1/200;
-        sphere.rotation.y -= 1/100;
-    }
     loader.onFinish = function () {
         engine.runRenderLoop(function () {
-            // hitbox.position = new BABYLON.Vector3(camera.position.x, camera.position.y, camera.position.z);
+            hitbox.position = new BABYLON.Vector3(camera.position.x, 0.2, camera.position.z);
+            // canDisplayMotion = hitbox.intersectsMesh(sphereModal, false);
+            // if ((canDisplayMotion) && guiMotion.style.display != "block") //Boucle pour savoir s'il faut afficher le texte pour dire qu'on peut interagir ou non
+            //     pressE.style.display = "block";
+            // else
+            //     pressE.style.display = "none"
+            // canHideSphere = hitbox.intersectsMesh(sphereModalTest, false);
+            // if (canHideSphere && mat_sphere3.alpha == 1 && !isShading) {
+            //     isShading = true
+            //     mat_sphere3.alpha = 0.99;
+            // } else if (canHideSphere && mat_sphere3.alpha != 1 && isShading) {
+            //     mat_sphere3.alpha -= 0.05;
+            // } else if (canHideSphere && mat_sphere3.alpha == 0 && isShading) {
+            //     sphere3.setEnabled(false);
+            //     isShading = false;
+            // }
             scene.render();
         });
     };
 
-    
+    window.addEventListener("keypress", function (e) {
+        // if (e.key == "e" && canDisplayMotion)
+        //     fenetreModal.style.display = "block";
+        if (e.key == "f") {
+            document.getElementsByTagName('body')[0].requestFullscreen();
+            document.getElementsByTagName('body')[0].style.cursor = 'none';
+        }
+        if (e.key == "g" || e.key == "escape") {
+            document.exitFullscreen();
+            document.getElementsByTagName('body')[0].style.cursor = 'auto';
+        }
+    });
+
     return scene; //Et on renvoie la scène pour pouvoir l'afficher plus tard.    
 }
 
 var scene = createScene();
 
 window.addEventListener('resize', function () { engine.resize() });
+
+// var fenetreModal = document.getElementById('fenetreModal'); //On récupère la fenêtre modale du fichier musee.html
+// var span = document.getElementsByClassName("close")[0] //On récupère la croix de la fenêtre modale
+// span.onclick = function () { fenetreModal.style.display = "none"; } //On cache la fenêtre modale si on clique sur la croix
+// window.onclick = function (event) {
+//     if (event.target == fenetreModal)
+//         fenetreModal.style.display = "none";
+// }//Si on clique sur la partie sombre, la fenêtre modale se fermera.
+
+// var pressE = document.getElementById('texteGUI');
+// var guiMotion = document.getElementById('fenetreModal');
